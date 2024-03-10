1db8600d11bd1497a5c0a06682462c45e39eca38
Kris Walker <kris.walker@icrar.org>
2024-03-11 02:45:06 +0800

    A significantly more efficient progenitor finder

--- orbitanalysis/progenitors.py
+++ orbitanalysis/progenitors.py
@@ -1,10 +1,8 @@
 import numpy as np
-from pathos.multiprocessing import ProcessingPool as Pool
-
-from orbitanalysis.utils import recenter_coordinates
+from orbitanalysis.utils import recenter_coordinates, myin1d
 
 
-def get_tracked_ids(snapshot):
+def get_tracked_ids(snapshot, n=100):
 
     slices = list(
         zip(snapshot.region_offsets[:-1], snapshot.region_offsets[1:]))
@@ -14,47 +12,44 @@ def get_tracked_ids(snapshot):
         for (start, end), p in zip(slices, snapshot.region_positions)], axis=0)
 
     rads = np.sqrt(np.einsum('...i,...i', region_coords, region_coords))
-    return [snapshot.ids[np.argsort(rads[start:end])[:100]+start]
-            for start, end in slices]
+    tracked_ids = [snapshot.ids[np.argsort(rads[start:end])[:n]+start]
+                   for start, end in slices]
+    offsets = np.cumsum([0] + [len(ids) for ids in tracked_ids])[:-1]
+    return np.hstack(tracked_ids), offsets
 
 
-def find_main_progenitors(halo_pids, tracked_ids, npool):
+def find_main_progenitors(halo_pids, halo_offsets, tracked_ids,
+                          tracked_offsets):
 
-    lengths = np.array([len(pids) for pids in halo_pids])
-    halonums = np.arange(len(halo_pids))
-    halo_pids_flat = np.hstack(halo_pids)
+    halo_diffs = np.diff(halo_offsets)
+    halo_lens = np.append(halo_diffs, len(halo_pids)-halo_offsets[-1])
+    tracked_diffs = np.diff(tracked_offsets)
+    tracked_lens = np.append(
+        tracked_diffs, len(tracked_ids)-tracked_offsets[-1])
 
-    tracked_ids_flat = np.hstack(tracked_ids)
-    present_all = np.in1d(halo_pids_flat, tracked_ids_flat, kind='table')
-    pcounts_all = np.split(present_all, np.cumsum(lengths))[:-1]
-    npresent_all = np.array([np.count_nonzero(pc) for pc in pcounts_all])
-    occupied = np.argwhere(npresent_all != 0).flatten()
-    unoccupied = np.argwhere(npresent_all == 0).flatten()
+    halo_number = np.hstack([
+        n * np.ones(hlen, dtype=int) for n, hlen in enumerate(halo_lens)])
 
-    halo_pids_occupied = [halo_pids[i] for i in occupied]
-    if len(halo_pids) == 0:
-        return None
-    halo_pids_flat = np.hstack(halo_pids_occupied)
-    lengths = np.delete(lengths, unoccupied)
-    halonums = np.delete(halonums, unoccupied)
+    intersect_inds = np.where(np.in1d(tracked_ids, halo_pids, kind='table'))[0]
+    tracked_ids_present = tracked_ids[intersect_inds]
+    tracked_ids_present = tracked_ids_present.astype(np.uint64)
 
-    def get_progenitor(ii):
-        ids = tracked_ids[ii]
-        present = np.in1d(halo_pids_flat, ids, kind='table')
-        pcounts = np.split(present, np.cumsum(lengths))[:-1]
-        npresent = [np.count_nonzero(pc) for pc in pcounts]
-
-        if np.all(npresent == 0):
-            return -1
+    inds = myin1d(halo_pids, tracked_ids_present, kind='table')
+    halo_numbers_progen_ = halo_number[inds]
+    halo_numbers_progen = -np.ones(len(tracked_ids), dtype=int)
+    halo_numbers_progen[intersect_inds] = halo_numbers_progen_
+    halo_numbers_progen_split = np.split(
+        halo_numbers_progen, np.cumsum(tracked_lens))[:-1]
+    halo_numbers_progen_split_noneg = [
+        hnums[hnums != -1] for hnums in halo_numbers_progen_split]
+    halo_number_counts = [
+        np.unique(hnums, return_counts=True) for hnums in
+        halo_numbers_progen_split_noneg]
+    haloids_new = []
+    for hnums_u, counts in halo_number_counts:
+        if len(hnums_u) == 0:
+            haloids_new.append(-1)
         else:
-            return halonums[np.argmax(npresent)]
-    if (npool == 1) or (npool is None):
-        haloids_new = []
-        for jj in range(len(tracked_ids)):
-            haloids_new.append(get_progenitor(jj))
-    else:
-        haloids_new = Pool(npool).map(
-            get_progenitor, np.arange(len(tracked_ids)))
-    haloids_new = np.array(haloids_new)
+            haloids_new.append(hnums_u[np.argmax(counts)])
 
     return haloids_new
